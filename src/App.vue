17ee97d369b7ec5c078389fc908d3eb189cd6f70
GSD111 <1399411420@qq.com>
2021-03-24 16:09:43 +0800

    '连接了吗?'

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
     <span class="footer__item">
       <div class="iconfont">&#xe621;</div>
       <div class="footer__title">我的</div>
+      <span>测试</span>
     </span>
   </div>
   <router-view/>

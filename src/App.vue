cfa7fadf50ccf7dd5b331d0a79ad46787845677d
GSD111 <1399411420@qq.com>
2021-03-25 15:08:00 +0800

    'login'

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Login />
   <div class="footer">
     <span class="footer__item footer__item--active">
       <div class="iconfont">&#xe658;</div>
@@ -20,31 +21,45 @@
   <router-view/>
 </template>
 
+<script>
+// import Login from '@/views/login/Login'
+
+export default {
+  name: 'App'
+  // eslint-disable-next-line vue/no-unused-components
+  // components: { Login }
+}
+</script>
+
 <style lang="scss">
-     .footer{
-       display:flex;
-       position: absolute;
-       left:0px;
-       //padding:0 .18rem;
-       bottom:0px;
-       width:100%;
-       height:.49rem;
-       border-top: solid  1px #f1f1f1;
-       &__item{
-         flex:1;
-         text-align:center;
-         .iconfont{
-               margin:0.07rem 0 0.02rem 0;
-               font-size:.2rem;
-             }
-         &--active{
-           color:#1fa4fc;
-         }
-       }
-       &__title{
-         font-size:.2rem;
-         transform: scale(.5 , .5);
-         transform-origin: center top;
-       }
-     }
+.footer {
+  display: flex;
+  position: absolute;
+  left: 0px;
+  //padding:0 .18rem;
+  bottom: 0px;
+  width: 100%;
+  height: .49rem;
+  border-top: solid 1px #f1f1f1;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: .2rem;
+    }
+
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+
+  &__title {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
 </style>

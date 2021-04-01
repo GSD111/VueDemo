85ef5f5a4b74ec55fa5afe3208d8dd5c3d460a55
GSD <1399411420@qq.com>
2021-04-01 22:53:29 +0800

    '购物车优化'

--- src/views/cartList/CartList.vue
+++ src/views/cartList/CartList.vue
@@ -1,15 +1,28 @@
 <template>
-<!--  <ProductList />-->
-  cart list
+  <!--  <ProductList />-->
+  <div class="header">
+    我的全部购物车
+  </div>
+  <div class="warning">
+    购物车空空如也哦~~~
+    <router-link to="/">
+      <div class="warning__btn">
+        去首页逛逛
+      </div>
+    </router-link>
+  </div>
+  <Footer />
 </template>
 
 <script>
+import Footer from '@/views/home/Footer'
 import ProductList from "@/views/orderConfirmation/ProductList";
 import getLocalCartList from '../../store/index'
-import { useRoute} from 'vue-router'
+import {useRoute} from 'vue-router'
+
 export default {
   name: 'CartList',
-  // components: {ProductList},
+  components: {Footer},
   // setup(){
   //   const router = useRoute()
   //   // console.log(router)
@@ -24,5 +37,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header {
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  color: #333;
+  text-align: center;
+  white-space: nowrap;
+}
+
+.warning {
+  position: absolute;
+  top: 40%;
+  left: 0;
+  right: 0;
+  text-align: center;
+  font-size: .16rem;
+  color: #e6e6e6;
+   a{
+     text-decoration: none;
+     color:#fff;
+   }
+  &__btn {
+    width: 1.1rem;
+    line-height: .4rem;
+    text-align: center;
+    margin: 0 auto;
+    font-size: .14rem;
+    background-color: #4FB0F9;
+    border-radius: .6rem;
+    color: #fff;
+    margin-top: .13rem;
+  }
+}
 
 </style>
